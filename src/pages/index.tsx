0dc1a3d135d14b932ad5dd018ac57ae46013c77e
gr153tt@gmail.com <gr153tt@gmail.com>
2021-09-06 22:01:54 -0300

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+import styles from '../styles/pages/Index.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  )
+}
