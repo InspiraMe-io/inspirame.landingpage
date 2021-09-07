811261abb57907aa748e611b597d0c0ba7144cc9
gr153tt@gmail.com <gr153tt@gmail.com>
2021-09-07 09:53:36 -0300

    finish header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,26 @@ import styles from '../styles/components/Header.module.scss'
 import { Button } from './Button'
 
 export default function Header() {
-
+    const scrollTo = (id: string) => {
+        const element = document.getElementById(id) as HTMLDivElement;
+        element.scrollIntoView({
+          behavior: "smooth",
+        });
+      };    
+    
     return (
         <header className={styles.headerContainer}>
             <div>
-                <div className={styles.logo} >
-                    Logo
-                </div>
+                <p>{"<"}inspireme{"/>"}</p>
             </div>
             <div>
                 <ul className={styles.navItems}>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Mission</a></li>
-                    <li><a href="#">Product</a></li>
-                    <li><Button text="Contact" /></li>
+                    <li className={styles.items}><a onClick={() => scrollTo("about")}>Sobre nós</a></li>
+                    <li className={styles.items}><a onClick={() => scrollTo("mission")}>Nossa missão</a></li>
+                    <li className={styles.items}><a href="#">Produto</a></li>
+                    <li  style={{ width: '180px'}}>
+                        <Button text="Contato" onClick={() => scrollTo("contact")} />
+                    </li>
                 </ul>
             </div>
         </header>
